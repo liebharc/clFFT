79f6c8784ab2e112c8517f79c1f72854381d3614
Christian Liebhardt <liebharc@users.noreply.github.com>
2017-02-06 21:46:45 -0800

    Started a Cargo project for clFFT so that hopefully Rust users only have to install cmake and to build this project with Cargo in order to use clFFT.

---
+++ build.rs
@@ -0,0 +1,10 @@
+extern crate cmake;
+
+use cmake::Config;
+
+fn main() {
+    // Call cmake to build clFFT
+    let _ = Config::new("src")
+                 .cflag("--build")
+                 .build();
+}
