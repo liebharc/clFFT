f56be40af0af146eff2cea89dc8656e4eb685c76
Christian Liebhardt <liebharc@users.noreply.github.com>
2017-02-11 22:21:50 -0800

    Removed bindgen dependency in the assumption that the FFI will be pretty stable and recreating the bindings every time makes the build to complicated

--- build.rs
+++ build.rs
@@ -1,19 +1,8 @@
 extern crate cmake;
-extern crate bindgen;
-
-use std::env;
-use std::path::Path;
 
 use cmake::Config;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    /*let _ = bindgen::builder()
-    .no_unstable_rust()
-    .header("src/include/clFFT.h")
-    .hide_type("cl_.*")
-    .generate().unwrap()
-    .write_to_file(Path::new(&out_dir).join("ffi.rs"));*/
     // Call cmake to build clFFT
     /*let _ = Config::new("src")
                  .cflag("--build")
